157a0684a117993117dd8646f1f95e4a960e8134
Sean <sdillon255@gmail.com>
2016-03-04 15:55:35 -0500

    initial carrier drag and drop

--- src/ShipMouseAdapter.java
+++ src/ShipMouseAdapter.java
@@ -10,6 +10,10 @@ public class ShipMouseAdapter extends MouseAdapter {
 
     static boolean shipBeingDragged;
     BattleshipGrid.CellPane[][] gridSpaces;
+    int releasedRow;
+    int releasedCol;
+    int boardHeight = 10;
+    int boardWidth = 10;
 
     public ShipMouseAdapter(boolean b, BattleshipGrid.CellPane[][] g){
         shipBeingDragged = b;
@@ -35,17 +39,25 @@ public class ShipMouseAdapter extends MouseAdapter {
 
         System.out.println("e.getX: "  + e.getX() + " e.getY: " + e.getY());
 
+
+
+
+
         if (e.getX() >= -500 && e.getX() <= 0 && e.getY() <= 500 && e.getY() >= 0) {
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
-                    if (e.getX() > gridSpaces[i][j].getX() && e.getX() < gridSpaces[i][j].getX() + 50) {
-                        if (e.getY() > gridSpaces[i][j].getY() && e.getY() < gridSpaces[i][j].getY() + 50) {
-                            System.out.println("grid cell at: " + i + " " + j);
-                            //TODO get proper row and col of the mouse release to mark as having a ship
-                        }
-                    }
-                }
+            releasedCol = 9 - (int)(Math.abs(e.getX()) / 50);
+            releasedRow = (int)(e.getY() / 50);
+            System.out.println("row: " + releasedRow);
+            System.out.println("col: " + releasedCol);
+
+            if(canDrop(releasedRow, releasedCol, 5)) {
+                //set the images
+                gridSpaces[releasedRow][releasedCol].setIcon(new ImageIcon("pics/carrier_1.png"));
+                gridSpaces[releasedRow + 1][releasedCol].setIcon(new ImageIcon("pics/carrier_2.png"));
+                gridSpaces[releasedRow + 2][releasedCol].setIcon(new ImageIcon("pics/carrier_3.png"));
+                gridSpaces[releasedRow + 3][releasedCol].setIcon(new ImageIcon("pics/carrier_4.png"));
+                gridSpaces[releasedRow + 4][releasedCol].setIcon(new ImageIcon("pics/carrier_5.png"));
             }
+
         }
 
 
@@ -62,6 +74,20 @@ public class ShipMouseAdapter extends MouseAdapter {
         }*/
     }
 
+    //helper functions
+
+    private boolean canDrop(int releasedRow, int releasedCol, int shipHight){
+
+        if(releasedRow <= (boardHeight - shipHight)){
+            //can drop
+
+        }else{
+            System.out.println("Cannot drop");
+            return false;
+        }
+
+        return true;
+    }
 
 
 
